b00671d5caa41fecbac7404763c24b41e74f7404
pavankumar263 <gajulapallepavankumarreddy@gmail.com>
2026-02-16 15:39:58 +0530

    UC1: Appliciation Entry & Welcome Message

---
+++ PalindromeCheckerAPPv1/src/PalindromeAPPv1.java
@@ -0,0 +1,15 @@
+/*
+ *Program starts.
+ *JVM invokes the main() method.
+ *Application name is displayed.
+ *Application version is displayed.
+ *Program continues to next use case or exits
+ * @author midhunreddy(jagan mavayya)
+ */
+
+
+public class PalindromeAPPv1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem Initalized......");
+    }
+}
