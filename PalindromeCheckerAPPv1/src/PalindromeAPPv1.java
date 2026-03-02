2bf303cb6936e577513f3dc7aba5c5ff171b4b38
ryvarma7 <ryvarma7@gmail.com>
2026-03-02 15:35:04 +0530

    Save progress on PalindromeAPPv1UC2

--- PalindromeCheckerAPPv1/src/PalindromeAPPv1.java
+++ PalindromeCheckerAPPv1/src/PalindromeAPPv1.java
@@ -1,15 +1,22 @@
-/*
- *Program starts.
- *JVM invokes the main() method.
- *Application name is displayed.
- *Application version is displayed.
- *Program continues to next use case or exits
- * @author midhunreddy(jagan mavayya)
- */
-
 
 public class PalindromeAPPv1 {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem Initalized......");
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
