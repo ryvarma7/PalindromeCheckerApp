08714d63e6ce2073d863a19168a578827cb8286d
ryvarma7 <ryvarma7gmail.com>
2026-03-10 14:52:39 +0530

    Add strategy pattern implementation for palindrome algorithms UC12

--- PalindromeCheckerAPPv1/src/PalindromeAPPv1.java
+++ PalindromeCheckerAPPv1/src/PalindromeAPPv1.java
@@ -1,33 +1,68 @@
 import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Stack;
 
 public class PalindromeAPPv1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string to check palindrome:");
         String input = sc.nextLine();
-        PalindromeChecker checker = new PalindromeChecker();
-        if (checker.checkPalindrome(input)) {
-            System.out.println("The string is a palindrome.");
+
+        PalindromeStrategy strategy;
+
+        // Choose strategy dynamically (example: StackStrategy)
+        strategy = new StackStrategy();
+        if (strategy.isPalindrome(input)) {
+            System.out.println("StackStrategy: The string is a palindrome.");
         } else {
-            System.out.println("The string is not a palindrome.");
+            System.out.println("StackStrategy: The string is not a palindrome.");
         }
+
+        // Another example: DequeStrategy
+        strategy = new DequeStrategy();
+        if (strategy.isPalindrome(input)) {
+            System.out.println("DequeStrategy: The string is a palindrome.");
+        } else {
+            System.out.println("DequeStrategy: The string is not a palindrome.");
+        }
+
         sc.close();
     }
 }
 
-class PalindromeChecker {
-    public boolean checkPalindrome(String str) {
-        String normalized = str.replaceAll("\\s+", "").toLowerCase();
-        return isPalindrome(normalized, 0, normalized.length() - 1);
-    }
+interface PalindromeStrategy {
+    boolean isPalindrome(String str);
+}
 
-    private boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
+class StackStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            stack.push(c);
         }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
+        for (char c : str.toCharArray()) {
+            if (stack.pop() != c) {
+                return false;
+            }
         }
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : str.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
